3b42ffe4e0ee18ad3b44fa90da3ce02c6bb2c707
namistarr <namistarr@icloud.com>
2019-08-05 15:22:28 -0700

    functional but without styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,97 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
+
+const itemData = [
+  {
+    name: 'Go over TK before lecture',
+    id: 1,
+    complete: false
+  },
+  {
+    name: 'Eat lunch',
+    id: 2,
+    complete: false
+  },
+  {
+    name: 'Work on todays project', 
+    id:3,
+    complete: false
+  },
+  {
+    name: 'Go first @ standup',
+    id: 4,
+    complete: false
+  },
+  {
+    name: 'Watch Buffy', 
+    id: 5,
+    complete: false
+  }
+]
+
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+      name: 'Jessica',
+      items: itemData
+    };    
+  }
+
+  toggleItem = id => {
+    console.log(id);
+    this.setState({
+      items: this.state.items.map(item => {
+        if (item.id === id) {
+          return {
+            ...item, 
+            complete: !item.complete
+          };
+        } else {
+          return item;
+        }
+      })
+    });
+  };
+
+  addItem = itemName => {
+    const newItem = {
+      name: itemName,
+      id: Date.now(), 
+      complete: false
+    };
+    this.setState({
+      items: [...this.state.items, newItem]
+    });
+  };
+
+  clearComplete = () => {
+    console.log('clear complete used')
+    this.setState({
+      items: this.state.items.filter(item => !item.complete)
+    });   
+  };
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <div className='header'>
+          <h2>Welcome to your Todo App!✍️</h2> 
+          <TodoForm 
+          addItem={this.addItem}           
+          />
+        </div>
+        <TodoList
+          items={this.state.items}
+          toggleItem={this.toggleItem}
+          clearComplete={this.clearComplete}
+        />
       </div>
     );
   }
